6ee53c47ba5a72257d3bbc105c49e9a3126b92c3
vegirevathi <vegirevathi@gmail.com>
2020-07-13 22:10:56 +0530

    ADDED: Functionality to check conversion of yard to feet

--- src/main/java/QuantityMeasurement/Service/QuantityMeasurement.java
+++ src/main/java/QuantityMeasurement/Service/QuantityMeasurement.java
@@ -3,6 +3,7 @@ package QuantityMeasurement.Service;
 public class QuantityMeasurement {
 
     private static int feetToInchConversionFactor = 12;
+    private static double yardToFeetConversionFactor = 3;
 
     public static boolean convertFeetToInch(double feet, double inches) {
         if (inches == feetToInchConversionFactor * feet) {
@@ -11,4 +12,13 @@ public class QuantityMeasurement {
             return false;
         }
     }
+
+    public static boolean convertYardToFeet(double yard, double feet) {
+        if (feet == yardToFeetConversionFactor * yard) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
