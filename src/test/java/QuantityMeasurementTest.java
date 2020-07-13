ab867c41ad36f41cd3a9c214bd880bbffae2dff5
vegirevathi <vegirevathi@gmail.com>
2020-07-13 06:17:23 +0530

    ADDED: Given values in feet should return values in inches

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import QuantityMeasurement.Service.QuantityMeasurement;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class QuantityMeasurementTest {
+    @Test
+    public void givenValuesInFeet_ShouldReturnCorrectValueInInches() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double feet = 2.0;
+        double inches = quantityMeasurement.convertFeetToInch(feet);
+        Assert.assertEquals(24, inches, 0.0);
+    }
 }
