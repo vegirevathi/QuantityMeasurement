9c1ef9a29b386add703a19abcc1005bd16cc3be8
vegirevathi <vegirevathi@gmail.com>
2020-07-13 21:32:41 +0530

    ADDED: Functionality to check feet to inch conversion

--- src/main/java/QuantityMeasurement/Service/QuantityMeasurement.java
+++ src/main/java/QuantityMeasurement/Service/QuantityMeasurement.java
@@ -1,9 +1,14 @@
 package QuantityMeasurement.Service;
 
 public class QuantityMeasurement {
-    public int FEET_TO_INCHES_CONVERSION_FACTOR = 12;
 
-    public double convertFeetToInch(double feet) {
-        return feet * FEET_TO_INCHES_CONVERSION_FACTOR;
+    private static int feetToInchConversionFactor = 12;
+
+    public static boolean convertFeetToInch(double feet, double inches) {
+        if (inches == feetToInchConversionFactor * feet) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
