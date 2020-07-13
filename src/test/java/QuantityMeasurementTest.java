0203dbac79a5e51bd8b09b2260b340f7cba055f0
vegirevathi <vegirevathi@gmail.com>
2020-07-13 22:09:53 +0530

    ADDED: Test case for comparing yard to feet

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -4,7 +4,17 @@ import org.junit.Test;
 
 public class QuantityMeasurementTest {
     @Test
-    public void givenValuesInFeet_ShouldReturnCorrectValueInInches() {
+    public void givenFeetAndInches_ShouldReturnComparisonResult() {
         Assert.assertEquals(true, QuantityMeasurement.convertFeetToInch(1.0, 12.0));
+    }
+
+    @Test
+    public void givenFeetAndInches_ShouldReturnComparisonResult_ForNull() {
+        Assert.assertEquals(true, QuantityMeasurement.convertFeetToInch(0.0, 0.0));
+    }
+
+    @Test
+    public void givenYardAndFeet_ShouldReturnComparisonResult() {
+        Assert.assertEquals(true, QuantityMeasurement.convertYardToFeet(1.0, 3.0));
     }
 }
