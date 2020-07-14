295190fd887d5236c4788b6f41648d105f21f1c8
vegirevathi <vegirevathi@gmail.com>
2020-07-14 18:13:43 +0530

    ADDED: Functionality for feet

--- src/main/java/QuantityMeasurement/Model/Feet.java
+++ src/main/java/QuantityMeasurement/Model/Feet.java
@@ -1,4 +1,18 @@
+package QuantityMeasurement.Model;
+
 public class Feet {
-    public Feet(double v) {
+    private final double value;
+
+    public Feet(double value) {
+        this.value = value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.value, value) == 0;
+    }
+
 }
