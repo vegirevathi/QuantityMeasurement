ad127a3fea49a594ef1e2808b4e60dc80f99b1b5
vegirevathi <vegirevathi@gmail.com>
2020-07-14 18:24:01 +0530

    ADDED: Test case to check given 0 feet and 1 feet are not equal

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -13,6 +13,13 @@ public class QuantityMeasurementTest {
     }
 
     @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
     public void givenFeetAndInches_ShouldReturnComparisonResult() {
         Assert.assertEquals(true, QuantityMeasurement.convertFeetToInch(1.0, 12.0));
     }
