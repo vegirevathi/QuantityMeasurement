19e5e3eefca3763b8a5d16d7b44d05c738c7012c
vegirevathi <vegirevathi@gmail.com>
2020-07-14 23:40:08 +0530

    ADDED: Functionality for centimeter

--- src/main/java/QuantityMeasurement/Model/Centimeter.java
+++ src/main/java/QuantityMeasurement/Model/Centimeter.java
@@ -1,4 +1,17 @@
+package QuantityMeasurement.Model;
+
 public class Centimeter {
-    public Centimeter(double v) {
+    public double value;
+
+    public Centimeter(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Centimeter that = (Centimeter) o;
+        return Double.compare(that.value, value) == 0;
     }
 }
