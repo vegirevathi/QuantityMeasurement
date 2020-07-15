a14429ee5e1aa66fce8128e5a9daa282652095d6
vegirevathi <vegirevathi@gmail.com>
2020-07-15 22:31:16 +0530

    REFACTORED: All test cases according to model class

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,7 +1,4 @@
-import QuantityMeasurement.Model.Centimeter;
-import QuantityMeasurement.Model.Feet;
-import QuantityMeasurement.Model.Inch;
-import QuantityMeasurement.Model.Yard;
+import QuantityMeasurement.Model.Length;
 import QuantityMeasurement.Service.QuantityMeasurement;
 import QuantityMeasurement.Utility.QuantityConversion;
 import org.junit.Assert;
@@ -11,212 +8,212 @@ public class QuantityMeasurementTest {
 
     @Test
     public void given0FeetAnd0Feet_ShouldReturnEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = new Length(Length.Unit.FEET, 0.0);
         Assert.assertEquals(feet1, feet2);
     }
 
     @Test
     public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = new Length(Length.Unit.FEET, 1.0);
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void given0FeetAndNull_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = null;
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = null;
         boolean result = feet1.equals(feet2);
         Assert.assertFalse(result);
     }
 
     @Test
     public void givenFeet1AndFeet2_FromDifferentTypes_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet("One");
+        Length feet1 = new Length(Length.Unit.FEET, 1.0);
+        String feet2 = "One";
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void givenFeet1AndFeet2_FromDifferentReferences_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        double feet2 = 0.0;      
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        double feet2 = 0.0;
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void given0InchAnd0Inch_ShouldReturnEqual() {
-        Inch Inch1 = new Inch(0.0);
-        Inch Inch2 = new Inch(0.0);
+        Length Inch1 = new Length(Length.Unit.INCH, 0.0);
+        Length Inch2 = new Length(Length.Unit.INCH, 0.0);
         Assert.assertEquals(Inch1, Inch2);
     }
 
     @Test
     public void given0InchAnd1Inch_ShouldReturnNotEqual() {
-        Inch Inch1 = new Inch(0.0);
-        Inch Inch2 = new Inch(1.0);
+        Length Inch1 = new Length(Length.Unit.INCH, 0.0);
+        Length Inch2 = new Length(Length.Unit.INCH, 1.0);
         Assert.assertNotEquals(Inch1, Inch2);
     }
 
     @Test
     public void given0InchAndNull_ShouldReturnNotEqual() {
-        Inch Inch1 = new Inch(0.0);
-        Inch Inch2 = null;
+        Length Inch1 = new Length(Length.Unit.INCH, 0.0);
+        Length Inch2 = null;
         Assert.assertNotEquals(Inch1, Inch2);
     }
 
     @Test
     public void given0InchAndZeroInch_ShouldReturnNotEqual() {
-        Inch Inch1 = new Inch(1.0);
-        Inch Inch2 = new Inch("One");
+        Length Inch1 = new Length(Length.Unit.INCH, 1.0);
+        String Inch2 = "One";
         Assert.assertNotEquals(Inch1, Inch2);
     }
 
     @Test
     public void given0InchAnd0Inch_FromDifferentClass_ShouldReturnNotEqual() {
-        Inch Inch1 = new Inch(0.0);
+        Length Inch1 = new Length(Length.Unit.INCH, 0.0);
         double Inch2 = 0.0;
         Assert.assertNotEquals(Inch1, Inch2);
     }
 
     @Test
     public void given0YardAnd0Yard_ShouldReturnEqual() {
-        Yard yard1 = new Yard(0.0);
-        Yard yard2 = new Yard(0.0);
+        Length yard1 = new Length(Length.Unit.YARD, 0.0);
+        Length yard2 = new Length(Length.Unit.YARD, 0.0);
         Assert.assertEquals(yard1, yard2);
     }
 
     @Test
     public void given0YardAnd1Yard_ShouldReturnNotEqual() {
-        Yard yard1 = new Yard(0.0);
-        Yard yard2 = new Yard(1.0);
+        Length yard1 = new Length(Length.Unit.YARD, 0.0);
+        Length yard2 = new Length(Length.Unit.YARD, 1.0);
         Assert.assertNotEquals(yard1, yard2);
     }
 
     @Test
     public void given0YardAndNull_ShouldReturnNotEqual() {
-        Yard yard1 = new Yard(0.0);
-        Yard yard2 = null;
+        Length yard1 = new Length(Length.Unit.YARD, 0.0);
+        Length yard2 = null;
         Assert.assertNotEquals(yard1, yard2);
     }
 
     @Test
     public void given0YardAnd0Yard_ClassChange_ShouldReturnNotEqual() {
-        Yard yard1 = new Yard(0.0);
+        Length yard1 = new Length(Length.Unit.YARD, 0.0);
         double yard2 = 1.0;
         Assert.assertNotEquals(yard1, yard2);
     }
 
     @Test
     public void given0CentimetersAnd0Centimeters_ShouldReturnEqual() {
-        Centimeter centimeter1 = new Centimeter(0.0);
-        Centimeter centimeter2 = new Centimeter(0.0);
+        Length centimeter1 = new Length(Length.Unit.CENTIMETER, 0.0);
+        Length centimeter2 = new Length(Length.Unit.CENTIMETER, 0.0);
         Assert.assertEquals(centimeter1, centimeter2);
     }
 
     @Test
     public void given0CentimetersAnd1Centimeters_ShouldReturnNotEqual() {
-        Centimeter centimeter1 = new Centimeter(0.0);
-        Centimeter centimeter2 = new Centimeter(1.0);
+        Length centimeter1 = new Length(Length.Unit.CENTIMETER, 0.0);
+        Length centimeter2 = new Length(Length.Unit.CENTIMETER, 1.0);
         Assert.assertNotEquals(centimeter1, centimeter2);
     }
 
     @Test
     public void given0CentimetersAndNull_ShouldReturnNotEqual() {
-        Centimeter centimeter1 = new Centimeter(0.0);
-        Centimeter centimeter2 = null;
+        Length centimeter1 = new Length(Length.Unit.CENTIMETER, 0.0);
+        Length centimeter2 = null;
         Assert.assertNotEquals(centimeter1, centimeter2);
     }
 
     @Test
     public void given0CentimetersAnd1Centimeters_DifferentReference_ShouldReturnNotEqual() {
-        Centimeter centimeter1 = new Centimeter(0.0);
+        Length centimeter1 = new Length(Length.Unit.CENTIMETER, 0.0);
         double centimeter2 = 0.0;
         Assert.assertNotEquals(centimeter1, centimeter2);
     }
 
     @Test
     public void givenFeetAndInches_WhenLengthsAreEqual_ShouldReturnTrue() {
-        Inch Inch = new Inch(12.0);
-        Feet feet = new Feet(1.0);
+        Length Inch = new Length(Length.Unit.INCH, 12.0);
+        Length feet = new Length(Length.Unit.FEET, 1.0);
         Assert.assertEquals(new QuantityMeasurement().getConversionValue(feet.value, QuantityConversion.FEET_TO_INCH), Inch.value, 0.000);
     }
 
     @Test
     public void givenFeetAndInches_WhenBothAreZero_ShouldReturnTrue() {
-        Inch Inch = new Inch(0.0);
-        Feet feet = new Feet(0.0);
+        Length Inch = new Length(Length.Unit.INCH, 0.0);
+        Length feet = new Length(Length.Unit.FEET, 0.0);
         Assert.assertEquals(new QuantityMeasurement().getConversionValue(feet.value, QuantityConversion.FEET_TO_INCH), Inch.value, 0.000);
     }
 
     @Test
     public void givenFeetAndInches_WhenLengthsAreNotEqual_ShouldReturnFalse() {
-        Inch Inch = new Inch(13.0);
-        Feet feet = new Feet(1.2);
+        Length Inch = new Length(Length.Unit.INCH, 13.0);
+        Length feet = new Length(Length.Unit.FEET, 1.2);
         Assert.assertNotEquals(new QuantityMeasurement().getConversionValue(feet.value, QuantityConversion.FEET_TO_INCH), Inch.value, 0.000);
     }
 
     @Test
     public void givenYardAndFeet_WhenLengthsAreEqual_ShouldReturnTrue() {
-        Yard yard = new Yard(1.0);
-        Feet feet = new Feet(3.0);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        Length feet = new Length(Length.Unit.FEET, 3.0);
         Assert.assertEquals(new QuantityMeasurement().getConversionValue(yard.value, QuantityConversion.YARD_TO_FEET), feet.value, 0.000);
     }
 
     @Test
     public void givenYardAndFeet_WhenLengthsAreNotEqual_ShouldReturnFalse() {
-        Yard yard = new Yard(1.0);
-        Feet feet = new Feet(2.99);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        Length feet = new Length(Length.Unit.FEET, 2.99);
         Assert.assertNotEquals(new QuantityMeasurement().getConversionValue(yard.value, QuantityConversion.YARD_TO_FEET), feet.value, 0.000);
     }
 
     @Test
     public void givenYardAndInches_WhenLengthsAreNotEqual_ShouldReturnFalse() {
-        Yard yard = new Yard(1.0);
-        Inch Inch = new Inch(13.0);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        Length Inch = new Length(Length.Unit.INCH, 13.0);
         Assert.assertNotEquals(new QuantityMeasurement().getConversionValue(yard.value, QuantityConversion.YARD_TO_INCH), Inch.value, 0.00);
     }
 
     @Test
     public void givenYardAndInches_WhenLengthsAreEqual_ShouldReturnTrue() {
-        Yard yard = new Yard(1.0);
-        Inch Inch = new Inch(36.0);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        Length Inch = new Length(Length.Unit.INCH, 36.0);
         Assert.assertEquals(new QuantityMeasurement().getConversionValue(yard.value, QuantityConversion.YARD_TO_INCH), Inch.value, 0.00);
     }
 
     @Test
     public void givenInchesAndYard_WhenLengthsAreEqual_ShouldReturnTrue() {
-        Inch Inch = new Inch(36.0);
-        Yard yard = new Yard(1.0);
+        Length Inch = new Length(Length.Unit.INCH, 36.0);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
         Assert.assertEquals(new QuantityMeasurement().getConversionValue(Inch.value, QuantityConversion.INCH_TO_YARD), yard.value, 0.1);
     }
 
     @Test
     public void givenInchesAndYard_WhenLengthsAreNotEqual_ShouldReturnFalse() {
-        Inch Inch = new Inch(36.0);
-        Yard yard = new Yard(1.2);
+        Length Inch = new Length(Length.Unit.INCH, 36.0);
+        Length yard = new Length(Length.Unit.YARD, 1.2);
         Assert.assertNotEquals(new QuantityMeasurement().getConversionValue(Inch.value, QuantityConversion.INCH_TO_YARD), yard.value, 0.1);
     }
 
     @Test
     public void givenFeetAndYard_WhenLengthsAreEqual_ShouldReturnTrue() {
-        Feet feet = new Feet(3.0);
-        Yard yard = new Yard(1.0);
+        Length feet = new Length(Length.Unit.FEET, 3.0);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
         Assert.assertEquals(new QuantityMeasurement().getConversionValue(feet.value, QuantityConversion.FEET_TO_YARD), yard.value, 0.1);
     }
 
     @Test
     public void givenFeetAndYard_WhenLengthsAreNotEqual_ShouldReturnFalse() {
-        Feet feet = new Feet(79.0);
-        Yard yard = new Yard(21.0);
+        Length feet = new Length(Length.Unit.FEET, 79.0);
+        Length yard = new Length(Length.Unit.YARD, 21.0);
         Assert.assertNotEquals(new QuantityMeasurement().getConversionValue(feet.value, QuantityConversion.FEET_TO_YARD), yard.value, 0.1);
     }
 
     @Test
     public void givenInchAndCentimeter_WhenLengthsAreEqual_ShouldReturnTrue() {
-        Inch inch = new Inch(1.0);
-        Centimeter centimeter = new Centimeter(2.5);
+        Length inch = new Length(Length.Unit.INCH, 1.0);
+        Length centimeter = new Length(Length.Unit.CENTIMETER, 2.5);
         Assert.assertEquals(new QuantityMeasurement().getConversionValue(inch.value, QuantityConversion.INCH_TO_CENTIMETER), centimeter.value, 0.1);
     }
 }
