0dfda745a4f68758e90da36c6351037785063c32
vegirevathi <vegirevathi@gmail.com>
2020-07-15 22:30:08 +0530

    REFACTORED: Created unique class for lenghts to avoid duplication of code

--- src/main/java/QuantityMeasurement/Service/QuantityMeasurement.java
+++ src/main/java/QuantityMeasurement/Service/QuantityMeasurement.java
@@ -4,9 +4,12 @@ import QuantityMeasurement.Utility.QuantityConversion;
 
 public class QuantityMeasurement {
 
-    private static int feetToInchConversionFactor = 12;
-    private static double yardToFeetConversionFactor = 3;
-    private static double yardToInchConversionFactor = 36;
+    /**
+     *
+     * @param unit
+     * @param quantityConversion
+     * @return
+     */
 
     public double getConversionValue(double unit, QuantityConversion quantityConversion){
         return unit*quantityConversion.CONVERSION_FACTOR;
