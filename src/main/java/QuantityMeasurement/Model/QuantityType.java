f45987c3a00642ee32fea4ad9939f79bac3aef72
vegirevathi <vegirevathi@gmail.com>
2020-07-16 19:06:44 +0530

    REFACTORED: Used single class for length,weight,volume and temperature

--- src/main/java/QuantityMeasurement/Model/QuantityType.java
+++ src/main/java/QuantityMeasurement/Model/QuantityType.java
@@ -2,13 +2,13 @@ package QuantityMeasurement.Model;
 
 import java.util.Objects;
 
-public class Weight {
-    public enum Unit{ GRAM, KILOGRAM, TONNE};
+public class QuantityType {
+    public enum Unit{ CELSIUS, FAHRENHEIT, FEET, INCH, YARD, CENTIMETER, GALLON, LITRE, MILLILITRE, GRAM, KILOGRAM, TONNE };
 
     private final Unit unit;
     public final double value;
 
-    public Weight(Unit unit, double value) {
+    public QuantityType(Unit unit, double value) {
         this.unit = unit;
         this.value = value;
     }
@@ -17,8 +17,8 @@ public class Weight {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Weight weight = (Weight) o;
-        return Double.compare(weight.value, value) == 0 &&
-                unit == weight.unit;
+        QuantityType that = (QuantityType) o;
+        return Double.compare(that.value, value) == 0 &&
+                unit == that.unit;
     }
 }
